14a5dcd77f2441c571136fb706c323e1bc712d50
benja27 <50673336+benja27@users.noreply.github.com>
2023-05-06 13:16:00 -0600

    first commit

---
+++ first.js
@@ -0,0 +1,12 @@
+const pets = ['Cat', 'Dog', 'Bird', 'Fish', 'Frog', 'Hamster', 'Pig', 'Horse', 'Lion', 'Dragon'];
+// Print all pets
+// console.log(pets[0]);
+// console.log(pets[1]);
+// console.log(pets[2]);
+// console.log(pets[3]);
+
+// for (let i = 0; i < pets.length; i++) {
+//   console.log(pets[i]);
+// }
+
+// then create a class name pets with the general styles which are font famuly and font size
